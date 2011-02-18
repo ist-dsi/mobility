8debf9d6a443ccd6935ffd7264dce3439a61608b
Susana Fernandes <susana.fernandes@ist.utl.pt>
2011-02-18 15:42:12 +0000

    Bug fix: fix getEmail

--- src/module/mobility/domain/PersonalPortfolio.java
+++ src/module/mobility/domain/PersonalPortfolio.java
@@ -16,73 +16,79 @@ import pt.ist.fenixWebFramework.services.Service;
 
 public class PersonalPortfolio extends PersonalPortfolio_Base {
 
-    public PersonalPortfolio(final Person person) {
-	super();
-	setMobilitySystem(MobilitySystem.getInstance());
-	setPerson(person);
-	setNotificationService(isEmployee(person));
-	new PersonalPortfolioProcess(this);
-    }
-
-    private Boolean isEmployee(Person person) {
-	for (Accountability accountability : person.getParentAccountabilities(IstAccountabilityType.PERSONNEL
-		.readAccountabilityType())) {
-	    if (accountability.isActiveNow()) {
-		return true;
-	    }
+	public PersonalPortfolio(final Person person) {
+		super();
+		setMobilitySystem(MobilitySystem.getInstance());
+		setPerson(person);
+		setNotificationService(isEmployee(person));
+		new PersonalPortfolioProcess(this);
 	}
-	return false;
-    }
 
-    @Service
-    public static PersonalPortfolio create(final Person person) {
-	return new PersonalPortfolio(person);
-    }
-
-    public PersonalPortfolioInfo getLastPersonalPortfolioInfo() {
-	return hasAnyPersonalPortfolioInfo() ? Collections.max(getPersonalPortfolioInfoSet()) : null;
-    }
-
-    public Set<WorkerOffer> getWorkerOffer() {
-	final Set<WorkerOffer> workerOffers = new TreeSet<WorkerOffer>();
-	for (final PersonalPortfolioInfo personalPortfolioInfo : getPersonalPortfolioInfoSet()) {
-	    workerOffers.addAll(personalPortfolioInfo.getWorkerOfferSet());
+	private Boolean isEmployee(Person person) {
+		for (Accountability accountability : person
+				.getParentAccountabilities(IstAccountabilityType.PERSONNEL
+						.readAccountabilityType())) {
+			if (accountability.isActiveNow()) {
+				return true;
+			}
+		}
+		return false;
 	}
-	return workerOffers;
-    }
 
-    public void addCurriculum(final PersonalPortfolioCurriculum personalPortfolioCurriculum) {
-	PersonalPortfolioInfo personalPortfolioInfo = getLastPersonalPortfolioInfo();
-	if (personalPortfolioInfo == null) {
-	    throw new NullPointerException("no.personalPortfolioCurriculum");
+	@Service
+	public static PersonalPortfolio create(final Person person) {
+		return new PersonalPortfolio(person);
 	}
-	if (!personalPortfolioInfo.canBeUpdated()) {
-	    personalPortfolioInfo = personalPortfolioInfo.duplicate();
+
+	public PersonalPortfolioInfo getLastPersonalPortfolioInfo() {
+		return hasAnyPersonalPortfolioInfo() ? Collections
+				.max(getPersonalPortfolioInfoSet()) : null;
 	}
-	personalPortfolioInfo.setPersonalPortfolioCurriculum(personalPortfolioCurriculum);
-    }
 
-    public String getEmail() {
-	// final RemotePerson remotePerson = getPerson().getRemotePerson();
-	// return remotePerson == null ? "" :
-	// remotePerson.getEmailForSendingEmails();
-	return "";
-    }
-
-    public Collection<Party> getWorkingPlaces() {
-	List<AccountabilityType> accountabilityTypes = new ArrayList<AccountabilityType>();
-	accountabilityTypes.add(IstAccountabilityType.PERSONNEL.readAccountabilityType());
-	accountabilityTypes.add(IstAccountabilityType.TEACHING_PERSONNEL.readAccountabilityType());
-	accountabilityTypes.add(IstAccountabilityType.RESEARCH_PERSONNEL.readAccountabilityType());
-	return getPerson().getParents(accountabilityTypes);
-    }
-
-    public boolean hasAnyActiveWorkerOfferOrPendingApproval() {
-	for (WorkerOffer workerOffer : getWorkerOffer()) {
-	    if (workerOffer.isActiveOrPendingApproval()) {
-		return true;
-	    }
+	public Set<WorkerOffer> getWorkerOffer() {
+		final Set<WorkerOffer> workerOffers = new TreeSet<WorkerOffer>();
+		for (final PersonalPortfolioInfo personalPortfolioInfo : getPersonalPortfolioInfoSet()) {
+			workerOffers.addAll(personalPortfolioInfo.getWorkerOfferSet());
+		}
+		return workerOffers;
 	}
-	return false;
-    }
+
+	public void addCurriculum(
+			final PersonalPortfolioCurriculum personalPortfolioCurriculum) {
+		PersonalPortfolioInfo personalPortfolioInfo = getLastPersonalPortfolioInfo();
+		if (personalPortfolioInfo == null) {
+			throw new NullPointerException("no.personalPortfolioCurriculum");
+		}
+		if (!personalPortfolioInfo.canBeUpdated()) {
+			personalPortfolioInfo = personalPortfolioInfo.duplicate();
+		}
+		personalPortfolioInfo
+				.setPersonalPortfolioCurriculum(personalPortfolioCurriculum);
+	}
+
+	public String getEmail() {
+		final RemotePerson remotePerson = getPerson().getRemotePerson();
+		return remotePerson == null ? "" : remotePerson
+				.getEmailForSendingEmails();
+	}
+
+	public Collection<Party> getWorkingPlaces() {
+		List<AccountabilityType> accountabilityTypes = new ArrayList<AccountabilityType>();
+		accountabilityTypes.add(IstAccountabilityType.PERSONNEL
+				.readAccountabilityType());
+		accountabilityTypes.add(IstAccountabilityType.TEACHING_PERSONNEL
+				.readAccountabilityType());
+		accountabilityTypes.add(IstAccountabilityType.RESEARCH_PERSONNEL
+				.readAccountabilityType());
+		return getPerson().getParents(accountabilityTypes);
+	}
+
+	public boolean hasAnyActiveWorkerOfferOrPendingApproval() {
+		for (WorkerOffer workerOffer : getWorkerOffer()) {
+			if (workerOffer.isActiveOrPendingApproval()) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
