ef91aff69deade5a3132d162d6a58ed821b3f469
Susana Fernandes <susana.fernandes@ist.utl.pt>
2012-02-29 12:18:49 +0000

    Minor Change: [Mobility] Changed accessibility groups

--- src/module/mobility/presentationTier/action/InterfaceCreationAction.java
+++ src/module/mobility/presentationTier/action/InterfaceCreationAction.java
@@ -15,11 +15,8 @@ import myorg.domain.RoleType;
 import myorg.domain.VirtualHost;
 import myorg.domain.contents.ActionNode;
 import myorg.domain.contents.Node;
-import myorg.domain.groups.IntersectionGroup;
-import myorg.domain.groups.NegationGroup;
 import myorg.domain.groups.PersistentGroup;
 import myorg.domain.groups.Role;
-import myorg.domain.groups.UnionGroup;
 import myorg.presentationTier.actions.ContextBaseAction;
 
 import org.apache.struts.action.ActionForm;
@@ -38,74 +35,34 @@ public class InterfaceCreationAction extends ContextBaseAction {
 	final VirtualHost virtualHost = getDomainObject(request, "virtualHostToManageId");
 	final Node node = getDomainObject(request, "parentOfNodesToManageId");
 
-	final PersistentGroup managementsGroup = MobilityGroup.getInstance();
-
-	// Mobility Managers
-	ActionNode homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "frontPage",
-		"resources.MobilityResources", "link.sideBar.mobility", managementsGroup);
-
-	ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "frontPage", "resources.MobilityResources",
-		"link.sideBar.mobility.frontPage", managementsGroup);
-
-	ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "portfolio", "resources.MobilityResources",
-		"link.sideBar.mobility.personalProfile", managementsGroup);
-
-	ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "jobOffers", "resources.MobilityResources",
-		"link.sideBar.mobility.jobOffers", managementsGroup);
-
-	ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "employeeOffers", "resources.MobilityResources",
-		"link.sideBar.mobility.employeeOffers", managementsGroup);
-
-	ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "configuration", "resources.MobilityResources",
-		"link.sideBar.mobility.configuration", Role.getRole(RoleType.MANAGER));
-
-	// Employees except Mobility Managers
-
 	OrganizationalModel organizationalModel = MobilitySystem.getInstance().getOrganizationalModel();
 	if (organizationalModel != null) {
-	    UnitGroup unitGroup = UnitGroup.getOrCreateGroup((Unit) organizationalModel.getPartiesIterator().next(),
+	    UnitGroup employeesGroup = UnitGroup.getOrCreateGroup((Unit) organizationalModel.getPartiesIterator().next(),
 		    new AccountabilityType[] { IstAccountabilityType.PERSONNEL.readAccountabilityType() },
 		    new AccountabilityType[] { IstAccountabilityType.ORGANIZATIONAL.readAccountabilityType() });
 
-	    final NegationGroup nonManagementsGroup = NegationGroup.createNegationGroup(managementsGroup);
-	    final IntersectionGroup mobilityPeopleGroup = IntersectionGroup.createIntersectionGroup(unitGroup,
-		    nonManagementsGroup);
+	    final PersistentGroup managementsGroup = MobilityGroup.getInstance();
 
-	    homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "portfolio", "resources.MobilityResources",
-		    "link.sideBar.mobility", mobilityPeopleGroup);
+	    ActionNode homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "jobOffers",
+		    "resources.MobilityResources", "link.sideBar.mobility", PersonGroup.getInstance());
+
+	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "frontPage", "resources.MobilityResources",
+		    "link.sideBar.mobility.frontPage", managementsGroup);
 
 	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "portfolio", "resources.MobilityResources",
-		    "link.sideBar.mobility.personalProfile", mobilityPeopleGroup);
+		    "link.sideBar.mobility.personalProfile", employeesGroup);
 
 	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "jobOffers", "resources.MobilityResources",
-		    "link.sideBar.mobility.jobOffers", mobilityPeopleGroup);
+		    "link.sideBar.mobility.jobOffers", PersonGroup.getInstance());
 
 	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "employeeOffers", "resources.MobilityResources",
-		    "link.sideBar.mobility.employeeOffers", mobilityPeopleGroup);
+		    "link.sideBar.mobility.employeeOffers", PersonGroup.getInstance());
 
 	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "configuration", "resources.MobilityResources",
 		    "link.sideBar.mobility.configuration", Role.getRole(RoleType.MANAGER));
 
-	    // Other Persons
-
-	    final UnionGroup alreadyDefined = UnionGroup.createUnionGroup(managementsGroup, mobilityPeopleGroup);
-	    final NegationGroup nonAlreadyDefinedGroup = NegationGroup.createNegationGroup(alreadyDefined);
-	    final IntersectionGroup otherPeopleGroup = IntersectionGroup.createIntersectionGroup(PersonGroup.getInstance(),
-		    nonAlreadyDefinedGroup);
-
-	    homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "jobOffers", "resources.MobilityResources",
-		    "link.sideBar.mobility", otherPeopleGroup);
-
-	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "jobOffers", "resources.MobilityResources",
-		    "link.sideBar.mobility.jobOffers", otherPeopleGroup);
-
-	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "employeeOffers", "resources.MobilityResources",
-		    "link.sideBar.mobility.employeeOffers", otherPeopleGroup);
-
-	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "configuration", "resources.MobilityResources",
-		    "link.sideBar.mobility.configuration", Role.getRole(RoleType.MANAGER));
 	} else {
-	    homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "configuration",
+	    ActionNode homeNode = ActionNode.createActionNode(virtualHost, node, "/mobility", "configuration",
 		    "resources.MobilityResources", "link.sideBar.mobility", Role.getRole(RoleType.MANAGER));
 	    ActionNode.createActionNode(virtualHost, homeNode, "/mobility", "configuration", "resources.MobilityResources",
 		    "link.sideBar.mobility.configuration", Role.getRole(RoleType.MANAGER));
